35aaacb105061e7749adb94d3f2faae4093ce5c9
Isaac <48419673+coffee-cake-isaac@users.noreply.github.com>
2024-09-10 00:56:05 -0400

    Safety check in

--- dayly/lib/main.dart
+++ dayly/lib/main.dart
@@ -9,10 +9,19 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        floatingActionButton: FloatingActionButton.extended(
+            onPressed: () {
+              print("Hello, world!");
+            },
+            label: const Text("New Task")),
+        body: CustomScrollView(
+          slivers: [
+            const SliverAppBar.large(
+              title: Text("Dayly"),
+            ),
+          ],
         ),
       ),
     );
